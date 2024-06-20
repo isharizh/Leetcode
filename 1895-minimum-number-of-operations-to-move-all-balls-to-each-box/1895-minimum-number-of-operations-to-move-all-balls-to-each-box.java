b8b96b39ec8a4b42a49323e5e232b5550274c227
Harish Kumar P <harizverse@gmail.com>
2024-06-20 13:04:06 +0530

    Time: 3 ms (96.58%), Space: 42.7 MB (100%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,34 @@
 class Solution {
-  public int[] minOperations(String boxes) {
-    int[] ans = new int[boxes.length()];
+    public int[] minOperations(String boxes) {
 
-    for (int i = 0, count = 0, moves = 0; i < boxes.length(); ++i) {
-      ans[i] += moves;
-      count += boxes.charAt(i) == '1' ? 1 : 0;
-      moves += count;
+        int n = boxes.length();
+        char ch[] = boxes.toCharArray();
+        int[] ans = new int[n];
+
+        int[] left = new int[n];
+        int count = ch[0]-'0';
+
+        for(int i =1;i<n;i++){
+            left[i] = left[i-1]+count;
+            count+=ch[i]-'0';
+
+
+        }
+
+        int[] right = new int[n];
+         count = ch[n-1]-'0';
+
+        for(int i =n-2;i>=0;i--){
+            right[i] =right[i+1]+count;
+            count+=ch[i]-'0';
+        }
+        
+
+      for(int i =0;i<n;i++){
+
+        ans[i] = left[i]+right[i];
+      }
+      System.gc();
+      return ans;
     }
-
-    for (int i = boxes.length() - 1, count = 0, moves = 0; i >= 0; --i) {
-      ans[i] += moves;
-      count += boxes.charAt(i) == '1' ? 1 : 0;
-      moves += count;
-    }
-
-    return ans;
-  }
 }
