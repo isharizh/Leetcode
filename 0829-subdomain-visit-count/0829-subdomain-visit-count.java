13a74571d62dedca1b74a8815e330f08e60f56be
Harish Kumar P <harizverse@gmail.com>
2024-06-20 16:26:02 +0530

    Time: 6 ms (100%), Space: 45.5 MB (38.22%) - LeetHub

--- 0829-subdomain-visit-count/0829-subdomain-visit-count.java
+++ 0829-subdomain-visit-count/0829-subdomain-visit-count.java
@@ -1,26 +1,41 @@
 class Solution {
-  
+
     public List<String> subdomainVisits(String[] cpdomains) {
-        Map<String, Integer> visitCounts = new HashMap<>();
-      
-        for (String domainInfo : cpdomains) {
-            int spaceIndex = domainInfo.indexOf(" ");
-            int visitCount = Integer.parseInt(domainInfo.substring(0, spaceIndex));
-          
-            for (int i = spaceIndex; i < domainInfo.length(); ++i) {
-                if (domainInfo.charAt(i) == ' ' || domainInfo.charAt(i) == '.') {
-                    String subdomain = domainInfo.substring(i + 1);
-                    visitCounts.put(subdomain, visitCounts.getOrDefault(subdomain, 0) + visitCount);
+        Map<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+
+        for (String cpDomain : cpdomains) {
+            calcVisit(cpDomain, map);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        map.forEach((domain, val) -> {
+            result.add(sb.append(val).append(" ").append(domain).toString());
+            sb.delete(0, sb.length());
+        });
+
+        return result;
+    }
+
+    private void calcVisit(String cpDomain, Map<String, Integer> map) {
+        int spaceIndex = 0;
+        while (cpDomain.charAt(spaceIndex) != ' ') {
+            spaceIndex += 1;
+        }
+
+        int count = Integer.parseInt(cpDomain.substring(0, spaceIndex));
+
+        for (int i = cpDomain.length() - 1; i >= spaceIndex; i--) {
+            if (cpDomain.charAt(i) == '.' || cpDomain.charAt(i) == ' ') {
+                String domain = cpDomain.substring(i + 1, cpDomain.length());
+                if (map.containsKey(domain)) {
+                    int oldVal = map.get(domain);
+                    map.replace(domain, oldVal + count);
+                } else {
+                    map.put(domain, count);
                 }
             }
         }
-      
-        List<String> results = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : visitCounts.entrySet()) {
-            // Format the entry as "visit_count domain" and add it to the results list.
-            results.add(entry.getValue() + " " + entry.getKey());
-        }
-      
-        return results;
+
     }
 }
