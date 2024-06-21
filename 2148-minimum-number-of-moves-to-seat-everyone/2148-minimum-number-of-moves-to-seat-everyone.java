bbece0577f3ce00673b97b5743be6b2071c2855a
Harish Kumar P <harizverse@gmail.com>
2024-06-21 15:48:49 +0530

    Time: 2 ms (89.44%), Space: 44.1 MB (44.55%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -1,29 +1,11 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        int moves1[] = new int[101];
-        int moves2[] = new int[101];
-        Arrays.fill(moves1,0);
-        Arrays.fill(moves2,0);
-        int n = seats.length;
-        for (int seat : seats) {
-            moves1[seat]++;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int count = 0;
+        for(int i=0;i<seats.length;i++){
+            count = count + (Math.abs(students[i]-seats[i]));
         }
-        for (int student : students) {
-            moves2[student]++;
-        }
-        int moves=0;int j=0,i=0;
-        //counting sort
-        while(n>0){
-            if(moves1[i]==0 ) i++;
-            if(moves2[j]==0 ) j++;
-            
-            if(moves1[i]!=0 && moves2[j]!=0){
-                moves+=Math.abs(i-j);
-                moves1[i]--; //can be multiple seats remove frequency
-                moves2[j]--;
-                n--;
-            }
-        }
-        return moves;
+        return count;
     }
 }
