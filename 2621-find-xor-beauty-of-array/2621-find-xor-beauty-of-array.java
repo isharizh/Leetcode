cef55c14518e28fb44ac872d077f8dc3cd35b92a
Harish Kumar P <harizverse@gmail.com>
2024-06-26 18:50:10 +0530

    Time: 1 ms (93.07%), Space: 55.3 MB (10.89%) - LeetHub

--- 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.java
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int xorBeauty(int[] nums) {
-        int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum ^= nums[i];
-        }  
-        return sum;   
+        int ans=0;
+        for(int n: nums){
+            ans^=n;
+        }
+        return ans;
     }
 }
