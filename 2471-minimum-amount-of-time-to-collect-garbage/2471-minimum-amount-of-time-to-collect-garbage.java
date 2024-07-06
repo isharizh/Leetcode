34907b1303b96f8da0770dd5528f80f644307507
Harish Kumar P <harizverse@gmail.com>
2024-07-06 14:49:55 +0530

    Time: 5 ms (90.68%), Space: 60.1 MB (71.03%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.java
@@ -1,38 +1,16 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int count = 0;
-        int mT= 0, pT = 0, gT = 0;
-        int lM = 0, lP = 0, lG = 0;
-        
-        for (int i = 0; i < garbage.length; i++) {
-            if (garbage[i].indexOf('M') != -1) {
-                long mCount = garbage[i].chars().filter(ch -> ch == 'M').count();
-                mT += mCount;
-                lM = i;
-            }
-            if (garbage[i].indexOf('P') != -1) {
-                long pCount = garbage[i].chars().filter(ch -> ch == 'P').count();
-                pT += pCount;
-                lP = i;
-            }
-            if (garbage[i].indexOf('G') != -1) {
-                long gCount = garbage[i].chars().filter(ch -> ch == 'G').count();
-                gT += gCount;
-                lG = i;
+        int count =0;
+        boolean m=false,g=false,p=false;
+        for(int i=garbage.length-1;i>=0;i--){
+            count +=garbage[i].length();
+            if(!m) m=garbage[i].contains("M");
+            if(!g) g=garbage[i].contains("G");
+            if(!p) p=garbage[i].contains("P");
+            if(i>0){
+                count += travel[i-1]*((m?1:0)+(p?1:0)+(g?1:0));
             }
         }
-        
-        for (int i = 0; i < lM; i++) {
-            mT += travel[i];
-        }
-        for (int i = 0; i < lP; i++) {
-            pT += travel[i];
-        }
-        for (int i = 0; i < lG; i++) {
-            gT += travel[i];
-        }
-        
-        count = mT + pT + gT;
         return count;
     }
 }
